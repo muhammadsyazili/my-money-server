fe2adc3cab73c030fe6dd79bff7b839df050539a
muhammadsyazili <09021181520135@students.ilkom.unsri.ac.id>
2021-03-08 00:00:13 +0700

    init commit

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $transactions = Transaction::orderByDesc('time')->get();
+        $response = [
+            'status' => Response::HTTP_OK,
+            'message' => 'List transaction order by time',
+            'data' => $transactions
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => ['required'],
+            'amount' => ['required', 'numeric'],
+            'type' => ['required', 'in:expense,revenue']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            $transaction = Transaction::create($request->all());
+            $response = [
+                'status' => Response::HTTP_OK,
+                'message' => 'Transaction created',
+                'data' => $transaction
+            ];
+
+            return response()->json($response, Response::HTTP_CREATED);
+        } catch (QueryException $qe) {
+            return response()->json([
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => "Failed $qe->errorInfo"
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        $response = [
+            'status' => Response::HTTP_OK,
+            'message' => 'Detail of transaction',
+            'data' => $transaction
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'title' => ['required'],
+            'amount' => ['required', 'numeric'],
+            'type' => ['required', 'in:expense,revenue']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            $transaction->update($request->all());
+            $response = [
+                'status' => Response::HTTP_OK,
+                'message' => 'Transaction updated',
+                'data' => $transaction
+            ];
+
+            return response()->json($response, Response::HTTP_OK);
+        } catch (QueryException $qe) {
+            return response()->json([
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => "Failed $qe->errorInfo"
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        try {
+            $transaction->delete();
+            $response = [
+                'status' => Response::HTTP_OK,
+                'message' => 'Transaction deleted'
+            ];
+
+            return response()->json($response, Response::HTTP_NO_CONTENT);
+        } catch (QueryException $qe) {
+            return response()->json([
+                'status' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => "Failed $qe->errorInfo"
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TransactionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Transaction::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word,
+            'amount' => $this->faker->buildingNumber,
+            'time' => now(),
+            'type' => $this->faker->randomElement(['expense', 'revenue']),
+        ];
+    }
+}
